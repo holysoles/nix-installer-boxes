bffc5c5c5d8ca1e5303e0975100a33bc34f32d1c
holysoles <holysoles97@gmail.com>
2025-12-11 22:32:04 -0600

    fix: create shell profile file

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -2,3 +2,7 @@
 
 apk update
 apk add tar xz sudo shadow
+
+# nix installer expects one of the common shell profile file. We'll make one
+touch /home/vagrant/.profile
+chown vagrant:vagrant /home/vagrant/.profile
